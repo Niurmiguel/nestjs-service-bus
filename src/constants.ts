afeb55aa8632e70fecaff6db59b5293f704d5e35
Niurmiguel <niurmiguelg@gmail.com>
2022-03-10 22:59:11 -0400

    [REF] remove routing service

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SB_INTERCEPTORS_METADATA = "sb_interceptors_metadata";
-export const SB_SUBSCRIBER_METADATA = "sb_subscriber_metadata";
+export const SB_INTERCEPTORS_METADATA = "service_bus_interceptors_metadata";
+export const SB_SUBSCRIBER_METADATA = "service_bus_subscriber_metadata";
+export const SB_EMITTER_METADATA = "service_bus_emitter_metadata";
 export const SB_MODULE_OPTIONS = "service_bus_module_options";
-export const SB_EMITTER_METADATA = "sb_emitter_metadata";

f36f5ed8bb116d71889978d0eee87b67b082f486
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-06-09 23:57:30 -0600

    feat(decorators): add Queue and Topic decorators

--- lib/decorators/subscriber.decorator.ts
+++ lib/decorators/subscriber.decorator.ts
@@ -1,9 +1,9 @@
 import { EventPattern } from "@nestjs/microservices";
-
 import { SB_SUBSCRIBER_METADATA } from "../azure-service-bus.constants";
 import { SbSubscriberMetadata } from "../metadata";
 import {
   MetaOrMetaFactory,
+  SbQueueMetadataOptions,
   SbSubscriptionMetadataOptions,
 } from "../interfaces";
 
@@ -44,3 +44,14 @@ export function Subscription(
     }
   };
 }
+
+export const Queue = (metadata: SbQueueMetadataOptions) => {
+  const data = { ...metadata, topic: metadata.queueName };
+  delete data.queueName;
+  return Subscription({
+    ...data,
+    subscription: null,
+  });
+};
+
+export const Topic = Subscription;
